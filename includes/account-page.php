13c1613432686e8de14347909c7d11857fa8d964
khadijahazward <khadijahazward@gmail.com>
2023-02-04 11:04:06 +0530

    updated accounts-page.php

--- includes/account-page.php
+++ includes/account-page.php
@@ -15,31 +15,54 @@ if (array_key_exists('action', $_GET)) {
 }
 
 $action = htmlspecialchars($action);
-$name = htmlspecialchars($_POST['name']);
-// POST actions
-if ($action == "add_account") {
 
-    $query = "SELECT * FROM accounts WHERE userid='" . $_SESSION['userid'] . "' AND accountname='" . $name . "'";
-    $result = $mysqli->query($query);
+// POST actions
+if ($action === "add_account") {
+
+    $query = "SELECT * FROM accounts WHERE userid=? AND accountname=?";
+    $stmt = $mysqli->prepare($query);
+    $stmt->bind_param("ss", $_SESSION['userid'], $name);
+    $stmt->execute();
+    $result = $stmt->get_result();
 
     if ($result->num_rows > 0) {
         echo "An account of that name already exists";
     } else {
-        $query = "INSERT INTO accounts (accountname, userid) VALUES ('" . $name . "', '" . $_SESSION['userid'] . "')";
-        $result = $mysqli->query($query);
-
-        if (!$result) {
-            echo "Failed to add account: (" . $mysqli->errno . ") " . $mysqli->error;
+        if (isset($_POST['name'])) {
+            $name = htmlspecialchars($_POST['name']);
+        
+            $query = "INSERT INTO accounts (accountname, userid, balance) VALUES (?, ?, 0)";
+            $stmt = $mysqli->prepare($query);
+            if (!$stmt) {
+                echo "Failed to prepare statement: (" . $mysqli->errno . ") " . $mysqli->error;
+                exit();
+            }
+            
+            $stmt->bind_param("ss", $name, $_SESSION['userid']);
+            $executionResult = $stmt->execute();
+            
+            if (!$executionResult) {
+                echo "Failed to add account: (" . $stmt->errno . ") " . $stmt->error;
+                exit();
+            }
+        } else {
+            echo "Error: The 'name' field is missing from the form data.";
         }
+        
+        echo "Account added successfully.";
         
     }
     
     $action = "view";
+
 } else if ($action == "upload") {
 
-    $query = "SELECT * FROM checks WHERE userid=" . $_SESSION['userid'] . " AND name='" . $name . "'";
+    $query = "SELECT * FROM checks WHERE userid=? AND name=?";
 
-    $result = $mysqli->query($query);
+    $stmt = $mysqli->prepare($query);
+    $stmt->bind_param("ss", $_SESSION['userid'], $name);
+    $stmt->execute();
+    $result = $stmt->get_result();
 
     if ($result->num_rows > 0) {
         echo "A check of that name already exists<br>";
@@ -49,21 +72,30 @@ if ($action == "add_account") {
             $upload_filename_items = explode(".", $_FILES['check_file']['name']);
             $ext = $upload_filename_items[count($upload_filename_items) - 1];
 
+            $ext = preg_replace("/[^a-zA-Z0-9\.]/", "", $ext);
+
             $filename = $_SESSION['user'] . "." . $name . "." . $ext;
             $src = $_FILES['check_file']['tmp_name'];
 
-            $full_path = dirname($_SERVER['SCRIPT_FILENAME']) . "/checks/" . $filename;
+            $full_path = dirname(__FILE__) . "/checks/" . $filename;
             $move_result = move_uploaded_file($src, $full_path);
+
             if ($move_result == TRUE) {
+                if (isset($_POST['Filename'])) {
+                    $name = htmlspecialchars($_POST['Filename']);
 
-                $insert = "INSERT INTO checks (userid, name, filename) VALUES ('" . $_SESSION['userid'] . "', '" . $name . "', '" . $filename . "')";
-                $result = $mysqli->query($insert);
+                    $insert = "INSERT INTO checks (userid, name, filename) VALUES (?, ?, ?)";
+                    $stmt = $mysqli->prepare($insert);
+                    $stmt->bind_param("sss", $_SESSION['userid'], $name, $filename);
+                    $stmt->execute();
 
-                if ($result !== FALSE) {
-                    echo "<div>Check uploaded!</div/>";
-                } else {
-                    echo "<div>Check insert failed!</div/>";
+                    if ($stmt) {
+                        echo "<div>Check uploaded!</div/>";
+                    } else {
+                        echo "<div>Check insert failed!</div/>";
+                    }
                 }
+
 
             } else {
                 echo "<div>Upload failed! (Perhaps the file was too big)</div/>";
@@ -77,18 +109,24 @@ if ($action == "add_account") {
     $action = "check_view";
 }
 
+
+
 if ($action == "logout") {
-    $_SESSION['logged_in'] = false;
+    // Clear all session data
+    $_SESSION = array();
+    session_destroy();
     echo "You have been logged out";
+
 } elseif ($action=="view") {
 
-    $query = "SELECT * FROM accounts WHERE userid='" . $_SESSION['userid'] . "'";
-
-    $result = $mysqli->query($query);
-
-	if (!$result) {
-		 echo "Failed to query: (" . $mysqli->errno . ") " . $mysqli->error;
-	}
+    $stmt = $mysqli->prepare("SELECT * FROM accounts WHERE userid=?");
+    $stmt->bind_param("i", $_SESSION['userid']);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    
+    if (!$result) {
+         echo "Failed to query: (" . $mysqli->errno . ") " . $mysqli->error;
+    }
     
     echo "<h3>Accounts</h3>\n<table class='table'>\n<tr><th>Account Name</th><th>Balance</th></tr>";
     
@@ -109,9 +147,12 @@ if ($action == "logout") {
      echo "<div class=\"alert alert-warning\" role=\"alert\">It may take awhile to process the check</div>";
 
     echo "<br><form method='post' action='account.php' enctype=\"multipart/form-data\"> ";
-    echo "<input type='text' name='name' />";
+    echo "File Name: <input type='text' name='Filename' required />";
+    echo "<br>";
     echo "<input type='file' name='check_file' />";
+    echo "<br>";
     echo "<input type='hidden' name='action' value='upload'/>";
+
     echo "<button class='btn btn-default'  type='submit'>Upload Check</button></form>";
     
 } elseif ($action=="check_view") {
@@ -128,7 +169,7 @@ if ($action == "logout") {
     
     if ($result->num_rows > 0) {
         while($row = $result->fetch_array(MYSQLI_ASSOC)){
-            echo "<tr><td>" . $row['name'] . "</td><td><a href='checks/" . $row['filename'] . "'>View</a></td></tr>";
+            echo "<tr><td>" . $row['name'] . "</td><td><a href='/ISS-Assignment/includes/checks/" . $row['filename'] . "'>View</a></td></tr>";
         }
     } else {
         echo "<tr><td>You have no checks</td><td>-</td></tr>";
